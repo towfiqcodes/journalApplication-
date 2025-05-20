e643949eaa1ddcf88e32dc63dbf468308896b540
mdtowfiqulislam <hridoy.islam7916@gmail.com>
2025-05-20 09:46:18 +0600

    Jwt Authentication and Authorization done

--- src/main/java/net/gaelixinfo/Journal/App/controller/PublicController.java
+++ src/main/java/net/gaelixinfo/Journal/App/controller/PublicController.java
@@ -1,16 +1,33 @@
 package net.gaelixinfo.Journal.App.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.gaelixinfo.Journal.App.entity.User;
+import net.gaelixinfo.Journal.App.service.UserDetailsServiceImpl;
 import net.gaelixinfo.Journal.App.service.UserService;
+import net.gaelixinfo.Journal.App.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck() {
@@ -18,9 +35,22 @@ public class PublicController {
     }
 
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user) {
         userService.saveNewUser(user);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+           authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+           userDetailsService.loadUserByUsername(user.getUsername());
+            String jwt = jwtUtil.generateToken(user.getUsername());
+            return  new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch (Exception e) {
+          log.error("Exception occured while createAuthenticationToken ", e);
+          return new ResponseEntity<>("Incorrect username or password", HttpStatus.UNAUTHORIZED);
+        }
+
+    }
 }
---
+++ src/main/java/net/gaelixinfo/Journal/App/utils/JwtUtil.java
@@ -0,0 +1,64 @@
+package net.gaelixinfo.Journal.App.utils;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    private String SECRET_KEY = "TaK+HaV^uvCHEFsEVfypW#7g9^k*Z8$V";
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<String, Object>();
+       return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().claims(claims)
+                .subject(subject)
+                .header().empty().add("typ","JWT")
+                .and()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ 1000 * 60*60))
+                .signWith(getSigningKey())
+                .compact();
+                
+
+    }
+
+    public Boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+}
